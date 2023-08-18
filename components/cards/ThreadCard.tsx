309644e9908e924ded6415172980e1fa2bad15ab
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-08-18 22:55:09 +0530

    Fetch Post From Home Page

---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,15 @@
+const ThreadCard = ({
+  key,
+  id,
+  currentUser,
+  parentId,
+  content,
+  author,
+  community,
+  createdAt,
+  comments,
+}) => {
+  return <div>ThreadCard</div>;
+};
+
+export default ThreadCard;
