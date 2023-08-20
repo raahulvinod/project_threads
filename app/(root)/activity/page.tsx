7844e8fc7d81ff6cc6fe30a7e1d24d4f68c18496
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-08-20 14:06:16 +0530

    Search Page Added

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,9 @@
+const page = async () => {
+  return (
+    <section>
+      <h1 className="head-text">Activity</h1>
+    </section>
+  );
+};
+
+export default page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,7 +5,7 @@ import { connectToDb } from '../mongoose';
 
 import User from '../models/user.model';
 import Thread from '../models/thread.model';
-import { model } from 'mongoose';
+import { FilterQuery, SortOrder } from 'mongoose';
 
 interface Params {
   userId: string;
@@ -84,5 +84,53 @@ export async function fetchUserPosts(userId: string) {
     return threads;
   } catch (error: any) {
     throw new Error(`Error fetch user posts: ${error.message}`);
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = '',
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = 'desc',
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const regex = new RegExp(searchString, 'i');
+
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId },
+    };
+
+    if (searchString.trim() !== '') {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return { users, isNext };
+  } catch (error: any) {
+    throw new Error(`Failed to fetch users: ${error.message}`);
   }
 }
