6c94ef4b90046b8c5d85b326b3f2c0df7bec796e
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-08-21 16:28:30 +0530

    Search Func

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,35 +1,40 @@
-import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
-import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
+import { currentUser } from '@clerk/nextjs';
 
 import UserCard from '@/components/cards/UserCard';
 
-const page = async () => {
-  const user = await currentUser();
+import Pagination from '@/components/shared/Pagination';
+import Searchbar from '@/components/shared/SearchBar';
 
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
-
   if (!userInfo?.onboarded) redirect('/onboarding');
 
-  // Fetch users
   const result = await fetchUsers({
     userId: user.id,
-    searchString: '',
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
 
   return (
     <section>
-      <h1 className="head-text">Search</h1>
+      <h1 className="head-text mb-10">Search</h1>
 
-      {/* Searh Bar */}
+      <Searchbar routeType="search" />
 
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
-          <p className="no-result">No users</p>
+          <p className="no-result">No Result</p>
         ) : (
           <>
             {result.users.map((person) => (
@@ -45,8 +50,14 @@ const page = async () => {
           </>
         )}
       </div>
+
+      <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </section>
   );
-};
+}
 
-export default page;
+export default Page;
