2b9b2903b893daa07b56e9790501f69806c75a93
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-08-21 13:34:25 +0530

    Onboarding typscript warning fixed

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,14 +1,18 @@
 import AccountProfile from '@/components/forms/AccountProfile';
+import { fetchUser } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
 async function Page() {
   const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
 
-  const userInfo = {};
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect('/');
 
   const userData = {
     id: user?.id,
-    objectId: userInfo?.id,
+    objectId: userInfo?._id,
     username: userInfo?.username || user?.username,
     name: userInfo?.name || user?.firstName || '',
     bio: userInfo?.bio || '',
